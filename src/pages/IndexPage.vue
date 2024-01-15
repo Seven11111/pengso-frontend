6b7e7ebb3b6450494753e7d8b10dcf1b07950efa
pzh <2584939303@qq.com>
2024-01-15 14:24:55 +0800

    前端主页完成

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="searchParams.text"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
+
+    <MyDivider />
+    <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
+      <a-tab-pane key="post" tab="文章">
+        <PostList :postList="postList" />
+      </a-tab-pane>
+      <a-tab-pane key="picture" tab="图片">
+        <PictureList />
+      </a-tab-pane>
+      <a-tab-pane key="user" tab="用户" force-render>
+        <UserList :user-list="userList" />
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import PostList from "@/components/PostList.vue";
+import UserList from "@/components/UserList.vue";
+import PictureList from "@/components/PictureList.vue";
+import MyDivider from "@/components/MyDivider.vue";
+import { useRoute, useRouter } from "vue-router";
+import myAxios from "../../plugins/myAxios";
+
+const router = useRouter();
+const route = useRoute();
+const activeKey = route.params.category;
+const initSearchParams = {
+  text: "",
+  pageSize: 10,
+  pageNum: 1,
+};
+const searchParams = ref(initSearchParams);
+const postList = ref([]);
+const userList = ref([]);
+
+myAxios.post("/post/list/page/vo", {}).then((res: any) => {
+  console.log(res);
+  postList.value = res.records;
+});
+
+myAxios.post("/user/list/page/vo", {}).then((res: any) => {
+  console.log(res);
+  userList.value = res.records;
+});
+
+watchEffect(() => {
+  searchParams.value = {
+    ...initSearchParams,
+    text: route.query.text,
+  } as any;
+});
+
+const onSearch = (value: string) => {
+  alert(value);
+  router.push({
+    query: searchParams.value,
+  });
+};
+
+const onTabChange = (key: string) => {
+  router.push({
+    path: `/${key}`,
+    query: searchParams.value,
+  });
+};
+</script>
